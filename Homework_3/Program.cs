0bf4962966e03c848bac0af1fcbb85e9de1f15c7
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-12-12 00:45:27 +0300

    Homework_3

--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -3,3 +3,68 @@
 // случайных целых чисел от 10 до 1000. (каждый эл-т массива – сгенерирован случайно)
 // [591, 532, 189, 523, 333, 546, 527, 275, 456, 264]
 
+
+// - - - ЕЩЕ РАБОТАЮ НАД ЗАДАНИЕМ - - - 
+
+// Пока  не понял как добавить генерацию чисел в цикл, массив заполняется одим числом.
+
+Random rnd = new Random(); 
+  
+int N = rnd.Next(10, 10000); 
+
+Console.WriteLine($"1 {N}");
+
+int n1 = N; 
+
+int n2 = N;
+
+int multiply = 1;
+
+int sum = 0;
+
+int [] array = new int[10];
+
+do{
+    multiply = multiply * (n1 % 10);
+    n1 = n1 / 10;
+    
+    sum = sum + n2 % 10;
+    n2 = n2 / 10;
+ }
+ while(n1 > 0 && n2 > 0);
+ {}
+ Console.WriteLine($"2 {multiply} и {sum}");
+
+ Console.WriteLine((float)multiply / sum);
+ 
+ Console.Write("[");
+
+for (int i = 0; i <= 9; i++)
+
+    {
+       if (multiply % sum == 0)
+       {
+       array[i] = N;  
+       Console.Write($"{array[i] }, ");
+       }                    
+    }
+
+Console.WriteLine("]");
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+ 
+// do{
+//     sum = sum + n2 % 10;
+//     n2 = n2 / 10;
+// }
+//  while(n2 > 0);
+//  {}
+//  Console.WriteLine($"3 {sum}");
