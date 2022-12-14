efcb3884e0648638a41a036db3db30ea751807a5
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-12-14 20:42:20 +0300

    Homework_3

--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -4,67 +4,59 @@
 // [591, 532, 189, 523, 333, 546, 527, 275, 456, 264]
 
 
-// - - - ЕЩЕ РАБОТАЮ НАД ЗАДАНИЕМ - - - 
+ int multiply(int N1)
+   {  int n1 = N1;
+      int multiply = 1;
+             
+        do{
+        multiply = multiply * (n1 % 10);
+        n1 = n1 / 10;
+        }
+        while(n1 > 0);
+        //Console.WriteLine($"Произведение всех цифры числа = {multiply}");
+        return(multiply);
+   }
+        
+int summ(int N2)
 
-// Пока  не понял как добавить генерацию чисел в цикл, массив заполняется одим числом.
-
-Random rnd = new Random(); 
+   {  int n2 = N2;
+      int sum = 0;
+        
+        do{
+        sum = sum + n2 % 10; 
+        n2 = n2 / 10;
+        }
+        while(n2 > 0);
+        //Console.WriteLine($"Сумма всех цифры числа = {sum}");
+        return(sum);
+   }
   
-int N = rnd.Next(10, 10000); 
 
-Console.WriteLine($"1 {N}");
-
-int n1 = N; 
-
-int n2 = N;
-
-int multiply = 1;
-
-int sum = 0;
-
-int [] array = new int[10];
-
-do{
-    multiply = multiply * (n1 % 10);
-    n1 = n1 / 10;
-    
-    sum = sum + n2 % 10;
-    n2 = n2 / 10;
- }
- while(n1 > 0 && n2 > 0);
- {}
- Console.WriteLine($"2 {multiply} и {sum}");
-
- Console.WriteLine((float)multiply / sum);
- 
- Console.Write("[");
+Console.Write("[");   
 
 for (int i = 0; i <= 9; i++)
-
-    {
-       if (multiply % sum == 0)
+{
+   Random rnd = new Random(); 
+   int N = rnd.Next(10, 10000); 
+   //Console.WriteLine($"генерируемое число{N}");
+     
+   int [] array = new int[10];   
+   int mult = N;
+   int sum = N;
+   mult = multiply(mult);
+   sum = summ(sum);
+   int result = mult % sum;
+   //Console.WriteLine($"Произведение цифр деленое с остатком на сумму цифр = {result}");
+   
+   
+       if (mult % sum == 0)
        {
        array[i] = N;  
        Console.Write($"{array[i] }, ");
+       }
+       else
+       {
+        i--;
        }                    
     }
-
 Console.WriteLine("]");
-
- 
- 
- 
- 
- 
- 
- 
- 
-   
- 
-// do{
-//     sum = sum + n2 % 10;
-//     n2 = n2 / 10;
-// }
-//  while(n2 > 0);
-//  {}
-//  Console.WriteLine($"3 {sum}");
