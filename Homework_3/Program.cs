0b95a2dac8a4529dec4d0eeb0dced2188f8c45a7
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-12-14 22:14:41 +0300

    Homework_3

--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -4,60 +4,83 @@
 // [591, 532, 189, 523, 333, 546, 527, 275, 456, 264]
 
 
- int product_digits_number(int N1)
-   {  int n1 = N1;
-      int multiply = 1;
+int product_digits_number(int N1)
+   {  
+   int n1 = N1;
+
+   int multiply = 1;
              
-        do{
-        multiply = multiply * (n1 % 10);
-        n1 = n1 / 10;
-        }
-        while(n1 > 0);
-        //Console.WriteLine($"Произведение всех цифры числа = {multiply}");
-        return(multiply);
+      do
+      {
+      multiply = multiply * (n1 % 10);
+      n1 = n1 / 10;
+      }
+      
+      while(n1 > 0);
+      
+      //Console.WriteLine($"Произведение всех цифры числа = {multiply}");
+      
+      return(multiply);
    }
         
 int  sum_digits_number(int N2)
 
-   {  int n2 = N2;
-      int sum = 0;
+   {  
+   int n2 = N2;
+   
+   int sum = 0;
         
-        do{
-        sum = sum + n2 % 10; 
-        n2 = n2 / 10;
-        }
-        while(n2 > 0);
-        //Console.WriteLine($"Сумма всех цифры числа = {sum}");
-        return(sum);
+      do
+      {
+      sum = sum + n2 % 10; 
+       n2 = n2 / 10;
+      }
+        
+      while(n2 > 0);
+        
+      //Console.WriteLine($"Сумма всех цифры числа = {sum}");
+        
+      return(sum);
    }
   
 
 Console.Write("[");   
 
 for (int i = 0; i <= 9; i++)
-{
+   {
    Random rnd = new Random(); 
+   
    int N = rnd.Next(10, 10000); 
+   
    //Console.WriteLine($"генерируемое число{N}");
      
    int [] array = new int[10];   
+   
    int mult = N;
+   
    int sum = N;
+   
    mult = product_digits_number(mult);
+   
    sum = sum_digits_number(sum);
+   
    int result = mult % sum;
+   
    //Console.WriteLine($"Произведение цифр деленое с остатком на сумму цифр = {result}");
    
    
-       if (mult % sum == 0)
-       {
-       array[i] = N;  
-       Console.Write($"{array[i] }, ");
-       }
-       else
-       {
-        i--;
-       }                    
-    }
+      if (mult % sum == 0)
+      {
+      array[i] = N;  
+      Console.Write($"{array[i] }, ");
+      }
+
+      else
+      {
+      i--;
+      }
+
+   }
+   
 Console.WriteLine("]");
 
